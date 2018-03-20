cbec61ffc6e0cc1cf42130129523af18edf973cf
alduma <37373309+alduma@users.noreply.github.com>
2018-03-20 09:06:01 -0500

    Update main.cpp

--- hello/main.cpp
+++ hello/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
-    cout<<"Bryan Pacheco Valdivia";
+    cout<<"Bryan Pacheco Valdivia"<<endl;
+    cout<<"alvaro durand"<<endl;
     return 0;
 }
