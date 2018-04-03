eaf629043abab051c78efdf65c2ed8567211a739
Helderheid <37373454+Helderheid@users.noreply.github.com>
2018-04-03 09:44:30 -0500

    Add files via upload
    trabajos

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout<<"Bryan Pacheco Valdivia"<<endl;
+    cout<<"alvaro durand"<<endl;
+    return 0;
+}
